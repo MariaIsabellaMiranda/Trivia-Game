3a92ea9bc6809a3b596c87930a742e2e903928e0
saraivais <isadorasaraiva95@gmail.com>
2022-05-24 15:40:45 -0300

    feat: pagina de login criada~ componentes de input controlados & botão sendo validado~

---
+++ src/pages/Login.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      mail: '',
+    };
+  }
+
+  handleInputChange = ({ target }) => {
+    const { id, value } = target;
+    this.setState({ [id]: value });
+  }
+
+  validateButton = () => {
+    const { name, mail } = this.state;
+    return !(name.length !== 0 && mail.length !== 0);
+  }
+
+  render() {
+    const { name, mail } = this.state;
+    return (
+      <section>
+        <label htmlFor="name">
+          <input
+            id="name"
+            type="text"
+            value={ name }
+            data-testid="input-player-name"
+            onChange={ this.handleInputChange }
+          />
+        </label>
+        <label htmlFor="mail">
+          <input
+            id="mail"
+            type="email"
+            value={ mail }
+            data-testid="input-gravatar-email"
+            onChange={ this.handleInputChange }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="btn-play"
+          disabled={ this.validateButton() }
+        >
+          Play
+
+        </button>
+      </section>
+    );
+  }
+}
+
+export default Login;
