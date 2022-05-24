cdf730cc9d3b47637ba3896b9f6325c0bee315d0
Vinicius de Medeiros <vmeedeiros@gmail.com>
2022-05-24 16:54:15 -0300

    Criação da página Feedback

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const MIN_ASSERTIONS = 3;
+
+class Feedback extends React.Component {
+  render() {
+    const { assertions } = this.props;
+    return (
+      <div>
+        <h2>Feedback Page</h2>
+        {
+          assertions.length >= MIN_ASSERTIONS ? (
+            <p data-testid="feedback-text">Could be better...</p>
+          ) : (
+            <p data-testid="feedback-text">Well Done!</p>
+          )
+        }
+      </div>
+    );
+  }
+}
+
+Feedback.propTypes = {
+  assertions: propTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+});
+
+export default connect(mapStateToProps)(Feedback);
