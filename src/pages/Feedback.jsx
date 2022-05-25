0f75e3cba64d477c0a6eb66c9479215daf0d5bde
Vinicius de Medeiros <vmeedeiros@gmail.com>
2022-05-25 16:35:48 -0300

    Implemento requisito 14 e 15

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -5,8 +5,13 @@ import { connect } from 'react-redux';
 const MIN_ASSERTIONS = 3;
 
 class Feedback extends React.Component {
+  redirectToLogin = () => {
+    const { history } = this.props;
+    history.push('/login');
+  }
+
   render() {
-    const { assertions } = this.props;
+    const { assertions, score } = this.props;
     return (
       <div>
         <h2>Feedback Page</h2>
@@ -17,6 +22,17 @@ class Feedback extends React.Component {
             <p data-testid="feedback-text">Could be better...</p>
           )
         }
+        <h4>Placar Final</h4>
+        <p data-testid="feedback-total-score">{score}</p>
+        <h4>Acertos</h4>
+        <p data-testid="feedback-total-question">{assertions}</p>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.redirectToLogin }
+        >
+          Play Again
+        </button>
       </div>
     );
   }
@@ -24,10 +40,13 @@ class Feedback extends React.Component {
 
 Feedback.propTypes = {
   assertions: propTypes.number.isRequired,
+  history: propTypes.shape().isRequired,
+  score: propTypes.number.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   assertions: state.player.assertions,
+  score: state.player.score,
 });
 
 export default connect(mapStateToProps)(Feedback);
